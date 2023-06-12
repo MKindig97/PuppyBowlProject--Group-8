7b5fcbba5c32664363997b4f759d84b868ca0207
Matt Kindig <mkindig97@gmail.com>
2023-06-12 16:34:43 -0400

    This is our final commit of the project.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const playerContainer = document.getElementById('all-players-container');
 const newPlayerFormContainer = document.getElementById('new-player-form');
-
+const detailsContainer = document.querySelector('#details-container');
 // Add your cohort name to the cohortName variable below, replacing the 'COHORT-NAME' placeholder
 const cohortName = '2302-ACC-ET-WEB-PT-A'; 
 // Use the APIURL variable for fetch requests
@@ -22,13 +22,14 @@ const fetchAllPlayers = async () => {
 };
 
 
-const fetchSinglePlayer = async (player) => { 
+const fetchSinglePlayer = async (id) => { 
     try {
-        const response = await fetch(`${APIURL}/${player}`);
+        const response = await fetch(`${APIURL}/${id}`);
         const singlePlayer = await response.json();
+        console.log(singlePlayer)
         return singlePlayer;
     } catch (err) {
-        console.error(`Oh no, trouble fetching player #${player}!`, err);
+        console.error(`Oh no, trouble fetching player #${id}!`, err);
     }
 };
 
@@ -54,7 +55,33 @@ const removePlayer = async (playerId) => {
         );
     }
 };
-
+// const renderBetterDetails = async (id) => {
+//     try {
+//         const player = await fetchSinglePlayer(id);
+//         playerContainer.innerHTML = '';
+//         const playerDetails = document.createElement('div');
+//         playerDetails.classList.add('player-detail');
+//           playerDetails.innerHTML = `
+//                     <h2>${player.name}</h2>
+//                     <p>${player.breed}</p>
+//                     <p>${player.status}</p>
+//                     <img src=${player.imageUrl} alt="Avatar" style="width:300px;height:300px;">
+//                     <button class="close-button" data-id="${player.id}">Close</button>
+//                 `;
+//           detailsContainer.appendChild(playerDetails);
+    
+//           // see details
+    
+//           const closeButton = playerDetails.querySelector('.details-button');
+//           closeButton.addEventListener('click', async (event) => {
+//             playerDetails.remove();
+//             playerContainer.style.visibility = "visible";
+//             // your code here
+//           });
+//     } catch (err) {
+//         console.error('Uh oh, trouble rendering players!', err);
+//     }
+// }
 /**
  * It takes an array of player objects, loops through them, and creates a string of HTML for each
  * player, then adds that string to a larger string of HTML that represents all the players. 
@@ -82,22 +109,28 @@ const renderAllPlayers = (playerList) => {
           const playerElement = document.createElement('div');
           playerElement.classList.add('player');
           playerElement.innerHTML = `
+          ${player.id}
                     <h2>${player.name}</h2>
                     <p>${player.breed}</p>
                     <img src=${player.imageUrl} alt="Avatar" style="width:300px;height:300px;">
                     <button class="details-button" data-id="${player.id}">See Details</button>
                     <button class="delete-button" data-id="${player.id}">Delete</button>
                 `;
-          playerContainer.appendChild(playerElement);
+        //   playerContainer.appendChild(playerElement);
     
           // see details
+    
           const detailsButton = playerElement.querySelector('.details-button');
           detailsButton.addEventListener('click', async (event) => {
-            renderSinglePlayerById(player.id);
+          const resp = await fetchSinglePlayer(player.id);
+          console.log(player.id + ' test BTN: ' + resp.data.player.name)
+            // playerContainer.style.visibility = "visible";
+            playerElement.innerHTML = `ID: ${resp.data.player.id}`
             // your code here
           });
+          playerContainer.appendChild(playerElement);
     
-          // delete party
+          // delete player
           const deleteButton = playerElement.querySelector('.delete-button');
           deleteButton.addEventListener('click', async (event) => {
             deletePlayer(player.id);
@@ -125,8 +158,11 @@ const renderNewPlayerForm = () => {
 const init = async () => {
     const players = await fetchAllPlayers();
     renderAllPlayers(players);
+    const asd = await fetchSinglePlayer(1);
+    console.log(asd);
     console.log(players);
     renderNewPlayerForm();
+  
 }
 
 init();
